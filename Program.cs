db7d05b61e61726d1cd934f80737e52d33080ebd
incimeltemd <incimeltemdaglaroglu@gmail.com>
2023-02-17 23:51:46 +0300

    dosya eklendi

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+try
+{
+    Console.WriteLine("bir sayı giriniz");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+}
+catch (Exception ex)
+{
+    Console.WriteLine("doğru bir sayı giriniz");
+    Console.WriteLine("hata:" + ex.Message.ToString());
+    
+}
+finally
+{
+    Console.WriteLine("işlem tamam");
+
+}
